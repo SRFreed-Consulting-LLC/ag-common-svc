316f636cac27f9cc0eb9fa5ef0a273e958b07ae2
Shane Freed <shane.freed@gmail.com>
2022-10-03 11:11:12 -0400

    moved to common project

---
+++ projects/ag-common-svc/src/lib/services/portal.service.ts
@@ -0,0 +1,42 @@
+import { Inject, Injectable } from '@angular/core';
+import { PortalData } from 'ag-common-lib/lib/models/utils/portal-data.model';
+import { DataService } from './data.service';
+import { FirebaseApp } from 'firebase/app';
+import { FIREBASE_APP } from '../injections/firebase-app';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PortalDataService extends DataService<PortalData>{
+  public collection: string = "portal-data";
+  
+  constructor(@Inject(FIREBASE_APP) fireBaseApp: FirebaseApp) {
+    super(fireBaseApp, null, null);
+    super.collection = 'agents';
+  }
+
+  
+
+  public getPortalData(): Promise<PortalData> {
+    return this.fsDao.getById(this.collection, 'data');
+  }
+  
+  public savePortalData(data: PortalData){
+    return this.fsDao.update(data, 'data', this.collection);
+  }
+
+  public getNextAgentId(): Promise<number>{
+    return new Promise(resolve => {
+      this.getPortalData().then(data => {
+        let portalData: PortalData = data;
+        let id: number = portalData.agentId;
+        
+        portalData.agentId = id + 1;
+
+        this.savePortalData(portalData);
+
+        resolve(id);
+      },err => console.error('Error in Portal Service', err))
+    })
+  }
+}

330cd61332e1c1782d7d1628f24763a3e98e1687
ASarokin <108413601+ASarokin@users.noreply.github.com>
2023-02-03 14:05:54 +0300

    additional fields

---
+++ projects/ag-common-svc/src/lib/components/agent-editor/components/agent-header/agent-header.model.ts
@@ -0,0 +1,6 @@
+export enum AgentHeaderKeys {
+  primaryEmailAddress = 'primaryEmailAddress',
+  primaryPhoneNumber = 'primaryPhoneNumber',
+  primaryShippingAddress = 'primaryShippingAddress',
+  primaryBillingAddress = 'primaryBillingAddress'
+}

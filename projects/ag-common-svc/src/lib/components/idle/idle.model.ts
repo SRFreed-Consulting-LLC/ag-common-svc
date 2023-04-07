c171139163788b46ee29f99e66cc691791ed2ddc
ASarokin <108413601+ASarokin@users.noreply.github.com>
2023-04-07 11:27:15 +0300

    hotfix

--- projects/ag-common-svc/src/lib/components/idle/idle.model.ts
+++ projects/ag-common-svc/src/lib/components/idle/idle.model.ts
@@ -1,8 +1,8 @@
 export const BROADCAST_CHANEL = 'IDLE_TIME_SERVICE_CHANEL' as const;
 
-export const DEFAULT_IDLE_TIME_DELAY = 0.1;
+export const DEFAULT_IDLE_TIME_DELAY = 1; // TODO replace after testing
 
-export const idleTimeDelaysLookup = [DEFAULT_IDLE_TIME_DELAY, 0.5, 30, 45];
+export const idleTimeDelaysLookup = [DEFAULT_IDLE_TIME_DELAY, 2, 30, 45];
 
 export enum IdleTimeServiceActions {
   logOut = 'LOG_OUT',
